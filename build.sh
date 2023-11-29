0b4cf7d11da898c9aad025384f6fe754a3c30c60
Homqyy <yilupiaoxuewhq@163.com>
2023-11-29 16:53:38 +0800

    improve build.sh

--- build.sh
+++ build.sh
@@ -4,18 +4,28 @@
 
 g_root_dir=`cd $(dirname $0); pwd`
 g_tongsuo_dir=$g_root_dir/tongsuo
+g_tongsuo_install_dir=/usr/local/tongsuo
 g_src_dir=$g_root_dir/src
 g_output_dir=$g_root_dir/output
 g_log_file=$g_output_dir/build.log
 g_debug=
+g_restart=
 g_sh=bash
+g_export_envs="CERTM_ROOT_DIR CERTM_OUTPUT_DIR \
+        CERTM_LOG_FILE CERTM_CONFIG_FILE \
+        CERTM_OPENSSL \
+        CERTM_CSR_CONF \
+        CERTM_ENC_CSR_CONF \
+        CERTM_ROOT_CA_DIR \
+        CERTM_SUB_CA_DIR \
+        CERTM_GM_ROOT_CA_DIR \
+        CERTM_GM_SUB_CA_DIR \
+        CERTM_CLIENT_DIR \
+        CERTM_SERVER_DIR"
 
 ################# Functions #################
 
-[ -d $g_output_dir ] || mkdir $g_output_dir
-[ -f $g_log_file ] || touch $g_log_file
-
-git submodule init && git submodule update >> $g_log_file 2>&1 || (echo "Failed to init submodules"; exit 1)
+git submodule init && git submodule update || (echo "Failed to init submodules"; exit 1)
 
 source $g_root_dir/tools-dev/base_for_bash.func
 
@@ -44,6 +54,10 @@ function clean
         [ -f .installed ] && rm .installed
 
         cd -
+
+        # TODO: uninstall tongsuo from system
+
+        # [ -d $g_tongsuo_install_dir ] && rm -rf $g_tongsuo_install_dir
     fi
 }
 
@@ -55,7 +69,8 @@ function build_tongsuo
 
     cd $g_tongsuo_dir
 
-    ./config --prefix=/usr/local/tongsuo -Wl,-rpath,/usr/local/tongsuo/lib enable-ec_elgamal enable-paillier enable-ntls >> $g_log_file 2>&1 \
+    # TODO: support install to system, but need sudo
+    ./config --prefix=$g_tongsuo_install_dir -Wl,-rpath,$g_tongsuo_dir enable-ec_elgamal enable-paillier enable-ntls >> $g_log_file 2>&1 \
         && make >> $g_log_file 2>&1 \
         || (cd -; return 1)
 
@@ -75,12 +90,12 @@ function build_certm
     export g_openssl=$g_tongsuo_dir/apps/openssl
     export g_csr_conf=$g_src_dir/assets/csr.conf
     export g_enc_csr_conf=$g_src_dir/assets/gm-enc-csr.conf
-    export g_root_ca_dir=$CERTM_OUTPUT_DIR/root-ca
-    export g_sub_ca_dir=$CERTM_OUTPUT_DIR/sub-ca
-    export g_gm_root_ca_dir=$CERTM_OUTPUT_DIR/gm-root-ca
-    export g_gm_sub_ca_dir=$CERTM_OUTPUT_DIR/gm-sub-ca
-    export g_client_dir=$CERTM_OUTPUT_DIR/clients
-    export g_server_dir=$CERTM_OUTPUT_DIR/servers
+    export g_root_ca_dir=$g_output_dir/root-ca
+    export g_sub_ca_dir=$g_output_dir/sub-ca
+    export g_gm_root_ca_dir=$g_output_dir/gm-root-ca
+    export g_gm_sub_ca_dir=$g_output_dir/gm-sub-ca
+    export g_client_dir=$g_output_dir/clients
+    export g_server_dir=$g_output_dir/servers
 
     [ -f $g_output_dir/.build ] && return 0
 
@@ -109,19 +124,24 @@ function install_certm
 
     # export environment variables to ~/.bashrc
 
-    echo "export CERTM_ROOT_DIR=$g_root_dir" >> ~/.bashrc
-    echo "export CERTM_OUTPUT_DIR=$g_output_dir" >> ~/.bashrc
-    echo "export CERTM_LOG_FILE=$g_log_file" >> ~/.bashrc
-    echo "export CERTM_CONFIG_FILE=$g_config_file" >> ~/.bashrc
-    echo "export CERTM_OPENSSL=$g_openssl" >> ~/.bashrc
-    echo "export CERTM_CSR_CONF=$g_csr_conf" >> ~/.bashrc
-    echo "export CERTM_ENC_CSR_CONF=$g_enc_csr_conf" >> ~/.bashrc
-    echo "export CERTM_ROOT_CA_DIR=$g_root_ca_dir" >> ~/.bashrc
-    echo "export CERTM_SUB_CA_DIR=$g_sub_ca_dir" >> ~/.bashrc
-    echo "export CERTM_GM_ROOT_CA_DIR=$g_gm_root_ca_dir" >> ~/.bashrc
-    echo "export CERTM_GM_SUB_CA_DIR=$g_gm_sub_ca_dir" >> ~/.bashrc
-    echo "export CERTM_CLIENT_DIR=$g_client_dir" >> ~/.bashrc
-    echo "export CERTM_SERVER_DIR=$g_server_dir" >> ~/.bashrc
+    CERTM_ROOT_DIR=$g_root_dir
+    CERTM_OUTPUT_DIR=$g_output_dir
+    CERTM_LOG_FILE=$g_log_file
+    CERTM_CONFIG_FILE=$g_config_file
+    CERTM_OPENSSL=$g_openssl
+    CERTM_CSR_CONF=$g_csr_conf
+    CERTM_ENC_CSR_CONF=$g_enc_csr_conf
+    CERTM_ROOT_CA_DIR=$g_root_ca_dir
+    CERTM_SUB_CA_DIR=$g_sub_ca_dir
+    CERTM_GM_ROOT_CA_DIR=$g_gm_root_ca_dir
+    CERTM_GM_SUB_CA_DIR=$g_gm_sub_ca_dir
+    CERTM_CLIENT_DIR=$g_client_dir
+    CERTM_SERVER_DIR=$g_server_dir
+
+    for e in $g_export_envs
+    do
+        echo "export $e=${!e}" >> ~/.bashrc
+    done
 
     echo "# certm install end: v1" >> ~/.bashrc
 }
@@ -180,15 +200,21 @@ if [ -n "$opt_uninstall" ]; then
         # remove install from ~/.bashrc
         sed -i '/# certm install start: v1/,/# certm insatll end: v1/d' ~/.bashrc
     fi
+    g_restart=1
+    d_success_info "Uninstall certm"
 fi
 
 if [ -n "$opt_clean" ]; then
     clean
-    d_success_info "Successfully clean all the build files"
-    exit 0
+    d_success_info "Clean all the build files"
 fi
 
 g_d_err_title="INIT"
+
+[ -d $g_output_dir ] || mkdir $g_output_dir
+[ -f $g_log_file ] || touch $g_log_file
+
+d_success_info "Init"
 
 g_d_err_title="BUILD"
 
@@ -205,6 +231,19 @@ g_d_err_title="INSTALL"
 if [ -n "$opt_install" ]; then
     install_certm
     d_success_info "Install certm"
+    g_restart=1
+fi
+
+g_d_err_title="DONE"
+
+if [ -n "$g_restart" ]; then
+    d_success_info "Restart bash"
+
+    # clean CERTM_* environment variables
+    for e in $g_export_envs
+    do
+        unset $e
+    done
+
     exec bash
-    exit 0
 fi
