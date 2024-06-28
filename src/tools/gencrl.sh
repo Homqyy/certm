12d73d54dec334c7021811b4599c527d7665f31d
Homqyy <yilupiaoxuewhq@163.com>
2024-06-28 21:13:17 +0800

    gen ca crl and crl chain

--- src/tools/gencrl.sh
+++ src/tools/gencrl.sh
@@ -12,6 +12,20 @@ cd $CERTM_PATH_SUB_CA_DIR
 $CERTM_BIN_OPENSSL ca -gencrl -config ca.conf -out $ca_dir/sub-ca.pem.crl -passin pass:root
 cd -
 
+cd $CERTM_PATH_ROOT_CA_DIR
+$CERTM_BIN_OPENSSL ca -gencrl -config ca.conf -out $ca_dir/ca.pem.crl -passin pass:root
+cd -
+
+cat $ca_dir/sub-ca.pem.crl > $ca_dir/ca-chain.pem.crl
+cat $ca_dir/ca.pem.crl >> $ca_dir/ca-chain.pem.crl
+
 cd $CERTM_PATH_GM_SUB_CA_DIR
 $CERTM_BIN_OPENSSL ca -gencrl -config ca.conf -out $ca_dir/gm-sub-ca.pem.crl -md sm3 -passin pass:root
 cd -
+
+cd $CERTM_PATH_GM_ROOT_CA_DIR
+$CERTM_BIN_OPENSSL ca -gencrl -config ca.conf -out $ca_dir/gm-ca.pem.crl -md sm3 -passin pass:root
+cd -
+
+cat $ca_dir/gm-sub-ca.pem.crl > $ca_dir/gm-ca-chain.pem.crl
+cat $ca_dir/gm-ca.pem.crl >> $ca_dir/gm-ca-chain.pem.crl
